9f879d29140358888d8cde05ceac6cff8e618189
“e <ldpiuaeng@163.com>
2019-12-11 23:23:46 +0800

    stash

---
+++ src/page/home/components/TestSlot.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <a :href="options">haha</a>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TestSlot',
+  data () {
+    return {
+      options: 'http://www.baidu.com'
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
